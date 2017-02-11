c6e679e79a97904a41a5d4b1f2315e1ac7aae023
eduardorost <eduardo.a.rost@gmail.com>
2017-02-11 15:59:23 -0200

    adicionando swagger

--- src/main/java/br/com/meutransporte/controllers/EventoController.java
+++ src/main/java/br/com/meutransporte/controllers/EventoController.java
@@ -3,18 +3,22 @@ package br.com.meutransporte.controllers;
 import br.com.meutransporte.models.Evento;
 import br.com.meutransporte.services.EventoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
-@RestController("v1/eventos")
+@Controller
+@RequestMapping("/v1/eventos")
 public class EventoController {
 
     @Autowired
     private EventoService eventoService;
 
-    @RequestMapping
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
     public List<Evento> getAll() {
         return eventoService.getAll();
     }
